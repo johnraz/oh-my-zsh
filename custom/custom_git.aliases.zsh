3910c50537d70115d462842c81b35d4ef3009824
Jonathan Liuti <liuti.john@gmail.com>
2013-10-07 01:19:02 +0200

    Added some missing git aliases

---
+++ custom/custom_git.aliases.zsh
@@ -0,0 +1,4 @@
+alias gll='git log --graph --pretty=oneline --abbrev-commit'
+compdef _git gll='git log --graph --pretty=oneline --abbrev-commit'
+alias gs='git status'
+compdef _git gs='git status'
